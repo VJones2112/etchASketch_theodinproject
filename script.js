ea309066ba25942ea93efe47495e833ac4de0937
Veronica Jones <veronicajones2112@gmail.com>
2020-08-24 17:35:17 -0500

    Added color effects. Need to figure out how to remove the style of that first column to which I appended all my rows. Still need to work on sizing as well.

--- script.js
+++ script.js
@@ -1,132 +1,50 @@
-// // Youtube tutorial from The Coding Train
+const div = document.getElementsByTagName('div');
+const tableDiv = document.getElementById('tableCanvas');
+const colorScales = document.querySelectorAll('.colors');
+let hue = 0;
 
-// let columns = 10;
-// let rows = 10;
-
-// function setup() {
-//     // createCanvas(300,300);
-//     colors = make2DArray(cols, rows);
-//     for (let i = 0; i < cols; i++) {
-//         for (let j = 0; j< rows; j++) {
-//             colors[i][j] = random(255)
-//         }
-//     }
-
-// }
-// // This didn't work
-// // function draw() {
-// //     document.body.style.background = ('lightblue no-repeat 100px');
-// //     for (i = 0; i < columns; i++) {
-// //         for(j = 0; j < rows; j++) {
-// //             let x = i * 30;
-// //             let y = i * 30;
-// //             // stroke(0);
-// //             j.body.style.background = ('255');
-// //             j.rect(x, y, 30, 30)
-// //         }
-
-// //     }
-
-// // }
-// // draw()
-
-
-// function make2DArray(cols, rows) {
-//     let arr = new Array(cols);
-//     for (let i = 0; i < arr.length; i++) {
-//         arr[i] = new Array(rows)
-//     }
-//     return arr;
-// }
-
-// make2DArray(5, 5)
-// // let sketchPad = () => document.getElementById('container').createElement('div');
-// // document.addEventListener('click', sketchPad)
-
-
-
-
-
-
-
-
-
-
-
-// // function sketchGrid(number) {
-// //     let grid = document.getElementById('container');
-// //     let gridDiv = document.createElement('div');
-    
-// //     for (i = 0; i < 5; i++) {
-// //         gridDiv = document.body.appendChild(gridDiv);
-// //         gridDiv.className = 'row'
-// //         // for(j = 0; j < 5; j++) {
-// //         //     let columnDiv = document.createElement('div');
-// //         //     columnDiv = document.body.appendChild(columnDiv);
-// //         //     columnDiv.className = 'column'
-// //         // }
-// //     }
-
-// // }
-
-// // sketchGrid(16)
-
-
-
-
-
-// Trying again on 8.17
-// Pseudo Code
-// Get User Input
-// get user input
-// store user input in variable
-// use variable to create grid
-// Create Grid Function
-// user input squared makes the grid
-// CSS makes grid flexbox or grid
-// Append the grid to Div
-
-
-// let getDiv = document.getElementById('container')
-// // let createDiv = document.createElement('div');
-// getDiv.innerHTML = '<div></div>'
-// // console.log(createDiv)
-// function createGrid(number) {
-//     for(let i = 0; i < number; i++) {
-//         let makeDivs = document.createElement('div');
-//         getDiv.appendChild(makeDivs);
-//     }
-    
-//     // addDiv = getDiv.innerHTML;
-//     // addDiv.append(getDiv);
-//     // console.log(createDiv)
-// } 
-// // console.log(createGrid)
-// createGrid(5);
-// console.log(createGrid(5))
-// document.getElementById('gridSize').addEventListener('click', createGrid);
-
-document.getElementById('gridSize').addEventListener('change', makeGrid);
-document.getElementById('resetGrid').addEventListener('click', resetGrid);
-
+// The Functions
 function makeGrid() {
-    let tbl = document.getElementById('tableCanvas');
+    tableDiv.innerHTML = "";
     let gridSize = document.getElementById('gridSize').value;
     for(let i = 0; i < gridSize; i++) {
-        let myRow = document.createElement('div')
+        let myRow = document.createElement('div'); // this is actually 1st columns
         myRow.id = 'row' + i;
-        tbl.appendChild(myRow);
-        let rowW = document.getElementById('row' + i);
-
+        myRow.classList.add('gridDiv');
+        //myRow.style.border = '0'; // Doesn't work
+        myRow.style.backgroundColor = 'orange'; 
+        tableDiv.appendChild(myRow);
+        let rowWidth = document.getElementById('row' + i);
         for(let j = 0; j < gridSize; j++) {
             let myCell = document.createElement('div');
-            rowW.appendChild(myCell);
-        }
-    }
+            rowWidth.appendChild(myCell);
+        };
+    };
 }
 
 function resetGrid() {
-    let removeDiv = document.getElementById('tableCanvas');
-    removeDiv.parentNode.removeChild(removeDiv)
-    console.log('Remove this')
+    tableDiv.innerHTML = "";
 }
+
+function changeColor(event) {
+    if (colorScale === 'grayScale') {
+        event.target.style.background = `hsl(0, 0%, ${hue}%)`;
+        hue += 0.5;
+            if (hue >= 99) {
+                hue = 0;
+            }
+    } 
+    if (colorScale === 'colorScale') {
+        event.target.style.background = `hsl(${hue}, 100%, 50%)`;
+        hue += 3;
+    }
+}
+
+
+// Event Listeners
+document.getElementById('gridSize').addEventListener('change', makeGrid);
+document.getElementById('resetGrid').addEventListener('click', resetGrid);
+document.getElementById('tableCanvas').addEventListener('mouseover', changeColor);
+colorScales.forEach(element => element.addEventListener('click', (e) => {
+    colorScale = element.id;
+}))
